2e21f6f5d9105524490c7eb8fb989fba4c7e30d2
Steve S. Lee <sjlee12@me.com>
2025-06-26 00:57:41 -0400

    ch 2 ex 1

--- chapter2/exercise.go
+++ chapter2/exercise.go
@@ -1,7 +1,28 @@
 package main
 
-import "log/slog"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+)
 
 func main() {
-	slog.Info("Exercise 2ß")
+	slog.Info("Exercise 2")
+	args := os.Args[1:]
+	for i := 0; i < len(args); i++ {
+		go one(args[i])
+	}
+	time.Sleep(2 * time.Second)
+}
+
+func one(filename string) {
+	fmt.Printf("Work %s started at %s\n", filename, time.Now().Format("15:04:05"))
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	slog.Info(string(data))
+	fmt.Printf("Work %s finished at %s\n", filename, time.Now().Format("15:04:05"))
 }
